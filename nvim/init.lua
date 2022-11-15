881e917640f6770b526faa5781b727686ad39279
Paweł Nowak <czesio@gmx.com>
2022-11-15 19:15:16 +0100

    nvim: Update config for new version of cmp_nvim_lsp

--- nvim/init.lua
+++ nvim/init.lua
@@ -163,8 +163,7 @@ local function enable_buf_organize_imports_on_save(bufnr)
   })
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local default_config = {
   capabilities = capabilities,
